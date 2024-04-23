a00f722fe84387ae681724bb6ba3936665cc7524
Abdumannof <Abdumannof>
2024-04-23 17:05:10 +0300

    Navigatsia paneli funksional holatga keldi va qaytadan commit qilindi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 
 const Hero = () => {
-  return (
-    <div>
-      Hero
-    </div>
-  );
+    return (
+        <div>
+            salom
+        </div>
+    );
 }
 
 export default Hero;
