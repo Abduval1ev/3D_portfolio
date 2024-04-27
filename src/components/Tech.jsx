3f8214f7b6c26845f909188b213df3a2308ccd18
Abdumannof <Abdumannof>
2024-04-27 18:39:46 +0300

    Ball paneli vorstka qilib bitqazildi + maumotlar map qilib olindi va funksional qilindi

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,7 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-col flex-wrap justify-center gap-10">
+    <div className="flex flex-wrap justify-between items-center gap-4 sm:gap-8 md:gap-10">
       {technologies.map((tech, index) => (
         <div className="w-28 h-28" key={index}>
           <BallCanvas icon={tech.icon} />
